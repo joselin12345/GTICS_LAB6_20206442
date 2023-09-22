cddc62bca2030ad4f015f1abd061a74638390cca
joselin <a20206442@pucp.edu.pe>
2023-09-21 20:35:23 -0500

    proyecto base

---
+++ lab6_gtics_20206442/src/main/java/com/example/lab6_gtics_20206442/Lab6Gtics20206442Application.java
@@ -0,0 +1,13 @@
+package com.example.lab6_gtics_20206442;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6Gtics20206442Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab6Gtics20206442Application.class, args);
+    }
+
+}
---
+++ lab6_gtics_20206442/src/test/java/com/example/lab6_gtics_20206442/Lab6Gtics20206442ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_gtics_20206442;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6Gtics20206442ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
